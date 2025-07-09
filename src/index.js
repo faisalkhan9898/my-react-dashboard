c9ccc0b7bfa9c2de4a6f6a7f71b991fb8dfd0a2b
faisal khan <faisalkhanbscit@gmail.com>
2025-07-09 13:30:30 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -4,55 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { HashRouter, Routes, Route } from 'react-router-dom';
+
 import Home from './common_componenet/Home';
 import Dashbord from './components/Dashbord';
 import Assignment from './components/Assignment';
 import Studymaterial from './components/Studymaterial';
-import Table from './components/Timetable';
 import Timetable from './components/Timetable';
 import Profiledetail from './components/Profiledetail';
 import ClassSub from './components/ClassSub';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const routes = createBrowserRouter([
-  {
-    path:'/',
-    element:<Home/>
-  },
-  {
-    path:'/Dashboard',
-    element:<Dashbord/>
-  },
-  {
-    path:'/Assignment',
-    element:<Assignment/>
-  },
-  {
-    path:'/Study-material',
-    element:<Studymaterial/>
-  },
-  {
-    path:'/timetable',
-    element:<Timetable/>
-  },
-   {
-     path:'/profile',
-     element:<Profiledetail/>
-   },
-   {
-    path:'/class-and-subject',
-    element:<ClassSub/>
-   }
-])
+
 root.render(
   <React.StrictMode>
-    <RouterProvider router={routes}/>
-    {/* <App /> */}
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashbord />} />
+        <Route path="/assignment" element={<Assignment />} />
+        <Route path="/study-material" element={<Studymaterial />} />
+        <Route path="/timetable" element={<Timetable />} />
+        <Route path="/profile" element={<Profiledetail />} />
+        <Route path="/class-and-subject" element={<ClassSub />} />
+      </Routes>
+    </HashRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
